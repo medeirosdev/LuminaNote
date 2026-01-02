0dab7fd6301c0956c7cdb1e2781a96b55f1d97c8
medeirosdev <guilherme.ggcm20@gmail.com>
2026-01-02 13:40:38 -0300

    feat: setup React app with Theme and Toast providers

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { ThemeProvider } from './hooks/useTheme.tsx'
+import { ToastProvider } from './hooks/useToast.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <ToastProvider>
+        <App />
+      </ToastProvider>
+    </ThemeProvider>
+  </StrictMode>,
+)
